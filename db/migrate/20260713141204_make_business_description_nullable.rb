9548358a755a5e0e18fdc7e1160c5dafccd686fa
Nicolas Müller <nicolas.mueller2@gmail.com>
2026-07-14 13:51:11 +0200

    fix: migrations

--- db/migrate/20260713141204_make_business_description_nullable.rb
+++ db/migrate/20260713141204_make_business_description_nullable.rb
@@ -1,5 +1,7 @@
 class MakeBusinessDescriptionNullable < ActiveRecord::Migration[8.0]
   def change
+    return unless column_exists?(:businesses, :description)
+
     change_column_null :businesses, :description, true
   end
 end
--- db/migrate/20260713141405_rename_businesses_name_to_contact_name.rb
+++ db/migrate/20260713141405_rename_businesses_name_to_contact_name.rb
@@ -1,5 +1,8 @@
 class RenameBusinessesNameToContactName < ActiveRecord::Migration[8.0]
   def change
+    return unless column_exists?(:businesses, :name)
+    return if column_exists?(:businesses, :contact_name)
+
     rename_column :businesses, :name, :contact_name
   end
 end
